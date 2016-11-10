46cbab1f46ed1a4cc44c29cf3bb1b1d094b1be95
roll <haozongqiang@foxmail.com>
2016-11-10 16:22:11 +0800

    添加jutil测试

--- src/main/java/com.comical/business/user/GetUserInfo.java
+++ src/main/java/com.comical/business/user/GetUserInfo.java
@@ -1,27 +1,28 @@
 package com.comical.business.user;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.Resource;
 
 import org.junit.Test;
 import org.springframework.orm.ibatis.SqlMapClientTemplate;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 /**
  * @author zongqiang.hao
  */
 
-@Repository
+@Service
 public class GetUserInfo {
 
     @Resource(name = "comicalEventSqlMapClientTemplate")
-    private SqlMapClientTemplate sqlMapClientTemplate;
+    private SqlMapClientTemplate comicalEventSqlMapClientTemplate;
 
     @Test
     public void getUser(){
-        List<User> userList;
-        userList =  sqlMapClientTemplate.queryForList("UserDao.getUser");
+        List<User> userList = new ArrayList<>();
+        userList =  comicalEventSqlMapClientTemplate.queryForList("ChushouUserProfileDao.getUser");
     }
 
 }
